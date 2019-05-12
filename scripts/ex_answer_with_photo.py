e9d6c5276b31044ab7508a1a4a36471877062246
Dmitry <dmitryhse@gmail.com>
2019-05-12 23:03:18 +0300

    add receive and send photos foundation

--- scripts/ex_answer_with_photo.py
+++ scripts/ex_answer_with_photo.py
@@ -9,10 +9,6 @@ token = '690091700:AAFEPFkipSkqkGtOnOouBc5lEYskqQiTiaU'
 # chat_id = get_chat_id(last_update(get_updates_json(url)))
 # send_mess(chat_id, 'Your message goes here')
 
-from numpy import random
-
-
-
 
 greet_bot = BotHandler(token=token)
 greetings = ('здравствуй', 'привет', 'ку', 'здорово')
@@ -25,33 +21,48 @@ def main():
     today = now.day
     hour = now.hour
 
+    # my_bot = Updater(settings.TELEGRAM_API_KEY)
+    # dp = my_bot.dispatcher
+    # dp.add_handler(CommandHandler("start", reply_to_start_command))
+    #     first_name = update.effective_user.first_name
+    #     update.message.reply_text("Пришли фото котика {}".format(first_name))
+
     while True:
-        greet_bot.get_updates(new_offset)
+        # greet_bot.get_updates(new_offset)
+        # last_update = greet_bot.get_last_update()
 
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
-        photo = open('photo.png', 'rb')
-        bot.send_photo(chat_id=last_chat_id, photo=photo)
+        # last_update_id = last_update['update_id']
+        # last_chat_text = last_update['message']['text']
+        # last_chat_id = last_update['message']['chat']['id']
+        # last_chat_name = last_update['message']['chat']['first_name']
+        # ------
+        # здесь update - элемент из get_updates. update.message.reply_text("??????????? ????")
+        updates = bot.get_updates()
+        for update in updates:
+            file = update.message.photo
+            if len(file) > 0:
+                file = file[0]
+                photo_file = bot.getFile(file) # получаем файл
+                photo_file.download('try.jpg')
+                photo = open('photo.png', 'rb')
+                bot.send_photo(chat_id=update.to_dict()['message']['chat']['id'], photo=photo)
 
 
-        new_offset = last_update_id + 1
+        # ------
+        #if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+        #    greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+        #    today += 1
+
+        #elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+        #    greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+        #    today += 1
+
+        #elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+        #    greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+        #    today += 1
+
+
+        #new_offset = last_update_id + 1
 
 
 if __name__ == '__main__':
